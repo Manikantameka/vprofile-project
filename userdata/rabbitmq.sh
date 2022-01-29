c8ed9e01de048a41119741778dd4158053f3a6aa
Manikantameka <manikantamekala.aws@gmail.com>
2022-01-29 23:19:48 +0530

    newcode

--- userdata/rabbitmq.sh
+++ userdata/rabbitmq.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 sudo yum install epel-release -y
+sudo yum install socat -y
+sudo yum install epel-release -y
+wget https://packages.erlang-solutions.com/erlang-solutions-1.0-1.noarch.rpm
+sudo rpm -Uvh erlang-solutions-1.0-1.noarch.rpm
+sudo yum install erlang -y
 sudo yum update -y
 sudo yum install wget -y
 cd /tmp/
-wget http://packages.erlang-solutions.com/erlang-solutions-2.0-1.noarch.rpm
-sudo rpm -Uvh erlang-solutions-2.0-1.noarch.rpm
-sudo yum -y install erlang socat
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
-sudo yum install rabbitmq-server -y
+wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.27/rabbitmq-server-3.8.27-1.el7.noarch.rpm
+rpm --import https://github.com/rabbitmq/signing-keys/releases/download/2.0/rabbitmq-release-signing-key.asc
+rpm -Uvh /tmp/rabbitmq-server-3.8.27-1.el7.noarch.rpm
 sudo systemctl start rabbitmq-server
 sudo systemctl enable rabbitmq-server
 sudo systemctl status rabbitmq-server
@@ -15,3 +18,7 @@ sudo sh -c 'echo "[{rabbit, [{loopback_users, []}]}]." > /etc/rabbitmq/rabbitmq.
 sudo rabbitmqctl add_user test test
 sudo rabbitmqctl set_user_tags test administrator
 sudo systemctl restart rabbitmq-server
+
+
+###https://github.com/rabbitmq
+###https://tecadmin.net/install-erlang-on-centos/
